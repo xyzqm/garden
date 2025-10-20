244d2f8f02b584e5964a4f85669756d018a46afb
Daniel Zhu <danielfangzhu@gmail.com>
2025-10-19 22:11:05 -0700

    change font size

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Latex(),
       Plugin.TikzJax(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -1,33 +1,15 @@
 import remarkMath from "remark-math"
 import rehypeKatex from "rehype-katex"
-import rehypeMathjax from "rehype-mathjax/svg"
 //@ts-ignore
-import rehypeTypst from "@myriaddreamin/rehype-typst"
 import { QuartzTransformerPlugin } from "../types"
 import { KatexOptions } from "katex"
-import { Options as MathjaxOptions } from "rehype-mathjax/svg"
 //@ts-ignore
-import { Options as TypstOptions } from "@myriaddreamin/rehype-typst"
 import { visit } from "unist-util-visit"
 import { Element } from "hast"
 import { toString } from "hast-util-to-string"
 import { typst2tex } from "tex2typst"
 
-interface Options {
-  renderEngine: "katex" | "mathjax" | "typst"
-  customMacros: MacroType
-  katexOptions: Omit<KatexOptions, "macros" | "output">
-  mathJaxOptions: Omit<MathjaxOptions, "macros">
-  typstOptions: TypstOptions
-}
-
-interface MacroType {
-  [key: string]: string
-}
-
-export const Latex: QuartzTransformerPlugin<Partial<Options>> = (opts) => {
-  const engine = opts?.renderEngine ?? "katex"
-  const macros = opts?.customMacros ?? {}
+export const Latex: QuartzTransformerPlugin = () => {
   return {
     name: "Latex",
     markdownPlugins() {
@@ -45,45 +27,25 @@ export const Latex: QuartzTransformerPlugin<Partial<Options>> = (opts) => {
                 : []
 
               if (classes.includes("language-math")) {
-                // console.log(toString(node))
-                // console.log(typst2tex(toString(node)))
-                // console.log(node)
                 node.children = [{ type: "text", value: typst2tex(toString(node)) }]
               }
             })
           }
         },
-        [rehypeKatex, { output: "html", macros, ...(opts?.katexOptions ?? {}) }],
+        [rehypeKatex, { output: "html" } as KatexOptions],
       ]
-      // switch (engine) {
-      //   case "katex": {
-      //     return [[rehypeKatex, { output: "html", macros, ...(opts?.katexOptions ?? {}) }]]
-      //   }
-      //   case "typst": {
-      //     return [[rehypeTypst, opts?.typstOptions ?? {}]]
-      //   }
-      //   case "mathjax": {
-      //     return [[rehypeMathjax, { macros, ...(opts?.mathJaxOptions ?? {}) }]]
-      //   }
-      //   default: {
-      //     return [[rehypeMathjax, { macros, ...(opts?.mathJaxOptions ?? {}) }]]
-      //   }
-      // }
     },
     externalResources() {
-      switch (engine) {
-        case "katex":
-          return {
-            css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
-            js: [
-              {
-                // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
-                src: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
-                loadTime: "afterDOMReady",
-                contentType: "external",
-              },
-            ],
-          }
+      return {
+        css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
+        js: [
+          {
+            // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
+            src: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
+            loadTime: "afterDOMReady",
+            contentType: "external",
+          },
+        ],
       }
     },
   }
