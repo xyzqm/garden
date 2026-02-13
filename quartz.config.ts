db34ce03555d57d5122f4f1be3b4f9fcb528fc8e
Daniel Zhu <danielfangzhu@gmail.com>
2026-02-12 23:28:53 -0800

    switch back to typst renderer...

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.Latex(),
+      Plugin.Latex({ renderEngine: "typst" }),
       Plugin.TikzJax(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -1,50 +1,75 @@
 import remarkMath from "remark-math"
 import rehypeKatex from "rehype-katex"
+import rehypeMathjax from "rehype-mathjax/svg"
 //@ts-ignore
+import rehypeTypst from "@myriaddreamin/rehype-typst"
 import { QuartzTransformerPlugin } from "../types"
 import { KatexOptions } from "katex"
+import { Options as MathjaxOptions } from "rehype-mathjax/svg"
 //@ts-ignore
-import { visit } from "unist-util-visit"
-import { Element } from "hast"
-import { toString } from "hast-util-to-string"
-import { typst2tex } from "tex2typst"
+import { Options as TypstOptions } from "@myriaddreamin/rehype-typst"
 
-export const Latex: QuartzTransformerPlugin = () => {
+interface Options {
+  renderEngine: "katex" | "mathjax" | "typst"
+  customMacros: MacroType
+  katexOptions: Omit<KatexOptions, "macros" | "output">
+  mathJaxOptions: Omit<MathjaxOptions, "macros">
+  typstOptions: TypstOptions
+}
+
+// mathjax macros
+export type Args = boolean | number | string | null
+interface MacroType {
+  [key: string]: string | Args[]
+}
+
+export const Latex: QuartzTransformerPlugin<Partial<Options>> = (opts) => {
+  const engine = opts?.renderEngine ?? "katex"
+  const macros = opts?.customMacros ?? {}
   return {
     name: "Latex",
     markdownPlugins() {
       return [remarkMath]
     },
     htmlPlugins() {
-      return [
-        () => {
-          return (tree) => {
-            // using tex2typst, convert all math blocks returned by remarkMath to latex
-            visit(tree, "element", (node: Element) => {
-              const classes = Array.isArray(node.properties.className)
-                ? node.properties.className
-                : []
-
-              if (classes.includes("language-math")) {
-                node.children = [{ type: "text", value: typst2tex(toString(node)) }]
-              }
-            })
-          }
-        },
-        [rehypeKatex, { output: "html", macros: { "\\xor": "\\oplus" } } as KatexOptions],
-      ]
+      switch (engine) {
+        case "katex": {
+          return [[rehypeKatex, { output: "html", macros, ...(opts?.katexOptions ?? {}) }]]
+        }
+        case "typst": {
+          return [[rehypeTypst, opts?.typstOptions ?? {}]]
+        }
+        default:
+        case "mathjax": {
+          return [
+            [
+              rehypeMathjax,
+              {
+                ...(opts?.mathJaxOptions ?? {}),
+                tex: {
+                  ...(opts?.mathJaxOptions?.tex ?? {}),
+                  macros,
+                },
+              },
+            ],
+          ]
+        }
+      }
     },
     externalResources() {
-      return {
-        css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
-        js: [
-          {
-            // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
-            src: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
-            loadTime: "afterDOMReady",
-            contentType: "external",
-          },
-        ],
+      switch (engine) {
+        case "katex":
+          return {
+            css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
+            js: [
+              {
+                // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
+                src: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
+                loadTime: "afterDOMReady",
+                contentType: "external",
+              },
+            ],
+          }
       }
     },
   }
